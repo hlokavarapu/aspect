d134aacb828ec4a461687d2591d54d04be57e246
Rene Gassmoeller <R.Gassmoeller@mailbox.org>
2015-02-05 19:35:30 +0100

    Compiler issues.

--- include/aspect/utilities.h
+++ include/aspect/utilities.h
@@ -307,13 +307,13 @@ namespace aspect
          * data we get from text files.
          */
         std::map<types::boundary_id,
-            std_cxx11::shared_ptr<::aspect::Utilities::AsciiDataLookup<dim-1> > > lookups;
+            std_cxx11::shared_ptr< ::aspect::Utilities::AsciiDataLookup<dim-1> > > lookups;
 
         /**
          * Map between the boundary id and the old data objects.
          */
         std::map<types::boundary_id,
-            std_cxx11::shared_ptr<::aspect::Utilities::AsciiDataLookup<dim-1> > > old_lookups;
+            std_cxx11::shared_ptr< ::aspect::Utilities::AsciiDataLookup<dim-1> > > old_lookups;
 
         /**
          * Handles the update of the data in lookup.
@@ -387,7 +387,7 @@ namespace aspect
          * Pointer to an object that reads and processes data we get from
          * text files.
          */
-        std_cxx11::shared_ptr<::aspect::Utilities::AsciiDataLookup<dim> > lookup;
+        std_cxx11::shared_ptr< ::aspect::Utilities::AsciiDataLookup<dim> > lookup;
 
     };
   }
