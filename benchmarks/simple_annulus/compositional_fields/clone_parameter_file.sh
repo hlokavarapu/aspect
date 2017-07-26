4b3f61b93ba3493a8c0e15800fc30b311f314ef1
Harsha Lokavarapu <lokavarapuh@gmail.com>
2017-07-26 16:35:51 -0700

    Iterate through finite element types for automation scripts

--- benchmarks/simple_annulus/compositional_fields/clone_parameter_file.sh
+++ benchmarks/simple_annulus/compositional_fields/clone_parameter_file.sh
@@ -4,9 +4,7 @@
 # Note that, one could add or delete to grid_resolution to run various models. 
 interpolation=('bilinear' 'cell_average')
 grid_resolution=('2', '3', '4', '5', '6')
-#list_of_number_of_particles_per_cell_in_x=('16')
-#list_of_number_of_particles_per_cell_in_y=('2','4','8','16','32','64','128')
-#list_of_number_of_particles_per_cell_in_total=($((list_of_number_of_particles_per_cell_in_x*list_of_number_of_particles_per_cell_in_y)),$((list_of_number_of_particles_per_cell_in_x*list_of_number_of_particles_per_cell_in_y)),$((list_of_number_of_particles_per_cell_in_x*list_of_number_of_particles_per_cell_in_y)),$((list_of_number_of_particles_per_cell_in_x*list_of_number_of_particles_per_cell_in_y)),$((list_of_number_of_particles_per_cell_in_x*list_of_number_of_particles_per_cell_in_y)),$((list_of_number_of_particles_per_cell_in_x*list_of_number_of_particles_per_cell_in_y)),$((list_of_number_of_particles_per_cell_in_x*list_of_number_of_particles_per_cell_in_y)))
+FEM_types=('Q1_P0' 'Q2_Q1' 'Q2_P-1' 'Q3_Q2' 'Q3_P-2')
 
 #These variables are user specific and will need to be appropriately changed:
 # 
@@ -20,55 +18,71 @@ parameter_template_file=`echo ${PWD}/${param_temp_filename}`
 # Make sure that each forward slash is delimted by a back slash. I.e. '\/'
 path_to_benchmark_library='./libsimple_annulus_compositional_fields.so'
 
-for interpolation_scheme in ${interpolation[@]}; do
-  if [ ! -d $interpolation_scheme ]; then
-    mkdir $interpolation_scheme
+for FEM_type in ${FEM_types[@]}; do
+  if [ ! -d $FEM_type ]; then
+    mkdir $FEM_type
+    echo $FEM_type 
   fi
-
-  cd $interpolation_scheme
-  for grid_res in ${grid_resolution[@]}; do
-    if [ ! -d $grid_res ]; then
-      mkdir $grid_res 
-    fi 
+  
+  top_level_dir=`echo $PWD`
+  cd $FEM_type
+  
+  for interpolation_scheme in ${interpolation[@]}; do
+    if [ ! -d $interpolation_scheme ]; then
+      mkdir $interpolation_scheme
+    fi
+  
+    cd $interpolation_scheme
     second_level_dir=`echo $PWD`
-    cd $grid_res
-    for n_p_x in ${list_of_number_of_particles_per_cell_in_x[@]}; do
-      mkdir $n_p_x
-      cd $n_p_x
-      echo $PWD
-      
-      cp ${parameter_template_file} ./${param_temp_filename}
 
+    for grid_res in ${grid_resolution[@]}; do
+      if [ ! -d $grid_res ]; then
+        mkdir $grid_res 
+      fi 
+
+      third_level_dir=`echo $PWD`
+      cd $grid_res
+
+      cp ${parameter_template_file} ./${param_temp_filename}
+  
       sed -i "s/set Additional shared libraries.*/set Additional shared libraries = ${path_to_benchmark_library}/" $param_temp_filename
       sed -i "s/set Output directory.*/set Output directory = $((grid_res))x$((grid_res))_$((n_p_x))_output/" $param_temp_filename  
+
       sed -i "s/set Initial global refinement.*/set Initial global refinement = $((grid_res))/" $param_temp_filename      
-       
-       if [ $interpolation_scheme == 'cell_average' ]; then
-         sed -i "s/set Interpolation scheme.*/set Interpolation scheme = cell average/" $param_temp_filename
-       elif [ $interpolation_scheme == 'bilinear' ]; then
-         sed -i "s/set Interpolation scheme.*/set Interpolation scheme = bilinear/" $param_temp_filename
-       elif [ $interpolation_scheme == 'biquadratic' ]; then
-         sed -i "s/set Interpolation scheme.*/set Interpolation scheme = biquadratic/" $param_temp_filename
-       fi
-   
-       min_x=`echo "1/($grid_res*$n_p_x*2)" | bc -l`
-       max_x=`echo "1 - 1/($grid_res*$n_p_x*2)" | bc -l`
-       min_y=`echo "1/($grid_res*$n_p_x*2)" | bc -l`
-       max_y=`echo "1 - 1/($grid_res*$n_p_x*2)" | bc -l`
-     
-       #sed -i "s/set Number of tracers.*/set Number of tracers = $((grid_res*grid_res*n_p_x*n_p_x))/" $param_temp_filename
-       #sed -i "s/set Number of particles in x.*/set Number of particles in x = $((grid_res*n_p_x))/" $param_temp_filename
-      # sed -i "s/set Number of particles in y.*/set Number of particles in y = $((grid_res*n_p_x))/" $param_temp_filename
-     
-       delta_x=`echo "1/($grid_res * $n_p_x)" | bc -l`
-       delta_y=`echo "1/($grid_res * $n_p_x)" | bc -l`
-     
-       sed -i "s/set Delta x.*/set Delta x = $delta_x/" $param_temp_filename
-       sed -i "s/set Delta y.*/set Delta y = $delta_y/" $param_temp_filename
-       
-       cd ..
-    done
+               
+      if [ $FEM_type == 'Q1_P0' ]; then
+        sed -i "s/set Composition polynomial degree.*/set Composition polynomial degree = 1/" $param_temp_filename
+        sed -i "s/set Stokes velocity polynomial degree.*/set Stokes velocity polynomial degree = 1/" $param_temp_filename
+        sed -i "s/set Use locally conservative discretization =.*/set Use locally conservative discretization = true/" $param_temp_filename
+      elif [ $FEM_type == 'Q2_Q1' ]; then
+        sed -i "s/set Composition polynomial degree.*/set Composition polynomial degree = 2/" $param_temp_filename
+        sed -i "s/set Stokes velocity polynomial degree.*/set Stokes velocity polynomial degree = 2/" $param_temp_filename
+        sed -i "s/set Use locally conservative discretization =.*/set Use locally conservative discretization = false/" $param_temp_filename
+      elif [ $FEM_type == 'Q2_P-1' ]; then
+        sed -i "s/set Composition polynomial degree.*/set Composition polynomial degree = 2/" $param_temp_filename
+        sed -i "s/set Stokes velocity polynomial degree.*/set Stokes velocity polynomial degree = 2/" $param_temp_filename
+        sed -i "s/set Use locally conservative discretization =.*/set Use locally conservative discretization = true/" $param_temp_filename
+      elif [ $FEM_type == 'Q3_Q2' ]; then
+        sed -i "s/set Composition polynomial degree.*/set Composition polynomial degree = 3/" $param_temp_filename
+        sed -i "s/set Stokes velocity polynomial degree.*/set Stokes velocity polynomial degree = 3/" $param_temp_filename
+        sed -i "s/set Use locally conservative discretization =.*/set Use locally conservative discretization = false/" $param_temp_filename
+      elif [ $FEM_type == 'Q3_P-2' ]; then
+        sed -i "s/set Composition polynomial degree.*/set Composition polynomial degree = 3/" $param_temp_filename
+        sed -i "s/set Stokes velocity polynomial degree.*/set Stokes velocity polynomial degree = 3/" $param_temp_filename
+        sed -i "s/set Use locally conservative discretization =.*/set Use locally conservative discretization = true/" $param_temp_filename
+      fi
+  
+      if [ $interpolation_scheme == 'cell_average' ]; then
+        sed -i "s/set Interpolation scheme.*/set Interpolation scheme = cell average/" $param_temp_filename
+      elif [ $interpolation_scheme == 'bilinear' ]; then
+        sed -i "s/set Interpolation scheme.*/set Interpolation scheme = bilinear/" $param_temp_filename
+      fi
+    
+      # Generate 16 particles per cell. 
+      sed -i "s/set Number of particles per cell per direction.*/set Number of particles per cell per direction = 4/" $param_temp_filename
+      cd $third_level_dir
+    done 
     cd $second_level_dir
-  done 
+  done
   cd $top_level_dir
 done
